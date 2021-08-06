20f9ba9354c880e12352847a8dbdb541c3aea181
shabin <shabin802@gmail.com>
2021-08-06 18:10:57 +0530

    new

--- backend/src/model/Resumedata.js
+++ backend/src/model/Resumedata.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/ResumeDB");
+mongoose.connect('mongodb+srv://userone:userone@ictakfiles.cpy6i.mongodb.net/LIBRARY?retryWrites=true&w=majority');
 const Schema=mongoose.Schema;
 
 const ResumeSchema=new Schema({
